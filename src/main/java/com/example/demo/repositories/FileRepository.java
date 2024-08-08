622a1d065a1809c628d3f6155bc38467ea672dc9
Ömer Feyzioğlu <oomerfeyzioglu@gmail.com>
2024-08-08 19:21:28 +0300

    repositories added

---
+++ src/main/java/com/example/demo/repositories/FileRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repositories;
+
+import com.example.demo.entity.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileRepository extends JpaRepository<File, Long> {
+
+    
+}
---
+++ src/main/java/com/example/demo/repositories/NoteRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repositories;
+
+import com.example.demo.entity.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+    List<Note> findByUserId(Long userId);
+
+}

3cc9010fddb949804dfe372d51b4f6e529a5561f
Ömer Feyzioğlu <oomerfeyzioglu@gmail.com>
2024-08-18 04:55:07 +0300

    all user operations completed

--- src/main/java/com/example/demo/controllers/NoteController.java
+++ src/main/java/com/example/demo/controllers/NoteController.java
@@ -21,38 +21,38 @@ public class NoteController {
     private final NoteService noteService;
     private final UserService userService;
 
-    @PostMapping
-    public ResponseEntity<Note> addNote(@RequestBody NoteDTO noteDTO ) {
-
-        Note note = new Note();
-        note.setTitle(noteDTO.getTitle());
-        note.setContent(noteDTO.getContent());
-
-        User user = userService.getUserById(noteDTO.getUserId());
-        note.setUser(user);
-
-        Note createdNote = noteService.createNote(note);
-
-        return new ResponseEntity<>(createdNote, HttpStatus.CREATED);
-
-
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Note> getNoteById(@PathVariable Long id) {
-        Note note = noteService.getNoteById(id);
-        return new ResponseEntity<>(note, HttpStatus.OK);
-    }
-
-    @GetMapping("/user/{userId}")
-    public ResponseEntity<List<Note>> getNotesByUserId(@PathVariable Long userId) {
-        List<Note> notes = noteService.getNotesByUserId(userId);
-        return new ResponseEntity<>(notes, HttpStatus.OK);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteNote(@PathVariable Long id) {
-        noteService.deleteNote(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
+//    @PostMapping
+//    public ResponseEntity<Note> addNote(@RequestBody NoteDTO noteDTO ) {
+//
+//        Note note = new Note();
+//        note.setTitle(noteDTO.getTitle());
+//        note.setContent(noteDTO.getContent());
+//
+//        User user = userService.getUserById(noteDTO.getUserId());
+//        note.setUser(user);
+//
+//        Note createdNote = noteService.createNote(note);
+//
+//        return new ResponseEntity<>(createdNote, HttpStatus.CREATED);
+//
+//
+//    }
+//
+//    @GetMapping("/{id}")
+//    public ResponseEntity<Note> getNoteById(@PathVariable Long id) {
+//        Note note = noteService.getNoteById(id);
+//        return new ResponseEntity<>(note, HttpStatus.OK);
+//    }
+//
+//    @GetMapping("/user/{userId}")
+//    public ResponseEntity<List<Note>> getNotesByUserId(@PathVariable Long userId) {
+//        List<Note> notes = noteService.getNotesByUserId(userId);
+//        return new ResponseEntity<>(notes, HttpStatus.OK);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<Void> deleteNote(@PathVariable Long id) {
+//        noteService.deleteNote(id);
+//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//    }
 }
--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,39 +1,57 @@
 package com.example.demo.controllers;
 
-import com.example.demo.configuration.UserMapper;
+
+import com.example.demo.configuration.FileMapper;
+import com.example.demo.dto.ResponseUserDTO;
 import com.example.demo.dto.UserDTO;
 import com.example.demo.entity.User;
 import com.example.demo.services.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-    private final UserMapper userMapper;
+    private final ModelMapper modelMapper;
+    private final Gson gson;
+    private final ObjectMapper objectMapper;
 
-    @PostMapping("/createUser")
-    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
-        UserDTO createdUserDTO = userService.createUser(userDTO);
-        return ResponseEntity.ok(createdUserDTO);
+    
+    @PostMapping("")
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO ) {
+       userService.createUser(userDTO);
+        return ResponseEntity.ok(userDTO);
     }
 
     @GetMapping
-    public ResponseEntity<List<UserDTO>> getAllUsers() {
-        List<User> users = userService.getAllUsers();
-        return ResponseEntity.ok(userMapper.toDTO(users));
+    public ResponseEntity<List<ResponseUserDTO>> getAllUsers() {
+        List<ResponseUserDTO> response  = userService.getAllUsers();
+        return  ResponseEntity.ok(response);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
-        User user = userService.getUserById(id);
-        return ResponseEntity.ok(userMapper.toDTO(user));
+    @GetMapping("/{id}")      //reponse entity type did not defined to be given errorMessage!
+    public ResponseEntity<?> getUserById(@PathVariable Long id) {
+      try {
+          ResponseUserDTO responseUserDTO = userService.getUserById(id);
+          return ResponseEntity.ok(responseUserDTO);
+      }catch (Exception e) {
+          String errorMessage = "There is no such user has this id!";
+          return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+      }
     }
 
     @DeleteMapping("/{id}")

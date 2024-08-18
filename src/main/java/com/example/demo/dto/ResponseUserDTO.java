fe3d42f5baab8569d426d9ad82b55e4517315178
Ömer Feyzioğlu <oomerfeyzioglu@gmail.com>
2024-08-18 04:56:12 +0300

    ResponseUserDTO added and UserDTO changed basically

---
+++ src/main/java/com/example/demo/dto/ResponseUserDTO.java
@@ -0,0 +1,18 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ResponseUserDTO {
+
+    private String username;
+
+    private LocalDateTime createdAt;
+
+
+
+
+
+}

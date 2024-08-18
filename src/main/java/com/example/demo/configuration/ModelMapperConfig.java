d9feeb0414f45314b4f46abe3788b22d77a5ed0a
Ömer Feyzioğlu <oomerfeyzioglu@gmail.com>
2024-08-18 04:54:09 +0300

    usermapper changed

--- src/main/java/com/example/demo/configuration/ModelMapperConfig.java
+++ src/main/java/com/example/demo/configuration/ModelMapperConfig.java
@@ -1,5 +1,7 @@
 package com.example.demo.configuration;
 
+import com.example.demo.dto.ResponseUserDTO;
+import com.example.demo.entity.User;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,10 +9,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ModelMapperConfig {
 
-    @Bean
+    @Bean("realMapper")
     public ModelMapper modelMapper() {
-
+        ModelMapper modelMapper = new ModelMapper();
         return new ModelMapper();
 
     }
+
 }
--- src/main/java/com/example/demo/configuration/UserMapper.java
+++ src/main/java/com/example/demo/configuration/UserMapper.java
@@ -1,44 +1,31 @@
-package com.example.demo.configuration;
-
-import com.example.demo.dto.UserDTO;
-import com.example.demo.entity.User;
-import org.modelmapper.ModelMapper;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-@Component
-public class UserMapper {
-
-    private final ModelMapper modelMapper;
-
-    public UserMapper(ModelMapper modelMapper) {
-        this.modelMapper = modelMapper;
-    }
-
-    public UserDTO toDTO(User user) {
-        UserDTO userDTO = modelMapper.map(user, UserDTO.class);
-        System.out.println("Converted UserDTO: " + userDTO);
-        return userDTO;
-    }
-
-    public User toEntity(UserDTO userDTO) {
-        User user = modelMapper.map(userDTO, User.class);
-        System.out.println("Converted User: " + user);
-        return user;
-    }
-
-
-    public List<UserDTO> toDTO(List<User> users) {
-        return users.stream()
-                .map(this::toDTO)
-                .collect(Collectors.toList());
-    }
-
-    public List<User> toEntity(List<UserDTO> userDTOs) {
-        return userDTOs.stream()
-                .map(this::toEntity)
-                .collect(Collectors.toList());
-    }
-}
+//package com.example.demo.configuration;
+//
+//import com.example.demo.dto.ResponseUserDTO;
+//import com.example.demo.entity.User;
+//import org.modelmapper.ModelMapper;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class UserMapper {
+//
+//    private final ModelMapper modelMapper;
+//
+//    @Autowired
+//    public UserMapper(ModelMapper modelMapper) {
+//        this.modelMapper = modelMapper;
+//    }
+//
+//    public ResponseUserDTO toDto(User user) {
+//        modelMapper.createTypeMap(User.class, ResponseUserDTO.class)
+//                .addMapping(User::getUsername, ResponseUserDTO::setUsername)       //you can set that the field you want to see in responseDTO list after added into DTO.
+//                .addMapping(User::getCreatedAt, ResponseUserDTO::setCreatedAt);
+////                .addMapping(User::getEmail, ResponseUserDTO::setEmail)
+//
+//        return modelMapper.map(user, ResponseUserDTO.class);
+//    }
+//
+//    public User toEntity(ResponseUserDTO userDTO) {
+//        return modelMapper.map(userDTO, User.class);
+//    }
+//}

c01caab25f2cac79c73ac983baefc0475d21dd3c
Ömer Feyzioğlu <oomerfeyzioglu@gmail.com>
2024-08-18 04:56:55 +0300

    UserService changed all db operations completed almost perfectly.

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,42 +1,62 @@
 package com.example.demo.services;
 
-import com.example.demo.configuration.UserMapper;
+
+//import com.example.demo.configuration.UserMapper;
+import com.example.demo.dto.ResponseUserDTO;
 import com.example.demo.dto.UserDTO;
 import com.example.demo.entity.User;
 import com.example.demo.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-    @Autowired
+
     private final UserRepository userRepository;
 
-    private final UserMapper userMapper;
+//    private final UserMapper userMapper;
 
-    public UserDTO createUser(UserDTO userDTO) {
+     @Qualifier("realMapper")
+    private final ModelMapper modelMapper;
 
-
-        User user = userMapper.toEntity(userDTO);
-
-        User createdUser = userRepository.save(user);
-         return userMapper.toDTO(createdUser);
+    public User createUser(UserDTO userDTO) {
+        User user = getUser(userDTO);
+        userRepository.save(user);
+         return user;
+    }
+    private User getUser(UserDTO userDTO) {
+        User user = modelMapper.map(userDTO, User.class);
+        user.setCreatedAt(LocalDateTime.now());
+        return user;
     }
 
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
+    public List<ResponseUserDTO> getAllUsers() {
+        List<ResponseUserDTO> responseUserDTO = userRepository.findAll().stream().map((element) -> modelMapper.map(element, ResponseUserDTO.class)).collect(Collectors.toList());
+        log.info("Retrieved users: {}", responseUserDTO);
+        return responseUserDTO;
     }
 
-    public User getUserById(Long id) {
+    public ResponseUserDTO getUserById(Long id) {
 
-        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        ResponseUserDTO responseUserDTO = getResponseUserDTO(user);
+        return responseUserDTO;
 
+    }
+    private ResponseUserDTO getResponseUserDTO(User user) {
+        ResponseUserDTO responseUserDTO = modelMapper.map(user, ResponseUserDTO.class);
+        return responseUserDTO;
     }
 
     public User getUserByEmail(String email) {

f80cefd888f9231d0b8cf8ec80532b377bad56d2
Ömer Feyzioğlu <oomerfeyzioglu@gmail.com>
2024-08-19 14:31:33 +0300

    all operations regarding with file and project completed

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,25 +1,15 @@
 package com.example.demo.controllers;
 
 
-import com.example.demo.configuration.FileMapper;
 import com.example.demo.dto.ResponseUserDTO;
 import com.example.demo.dto.UserDTO;
-import com.example.demo.entity.User;
 import com.example.demo.services.UserService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/user")
--- src/main/java/com/example/demo/entity/File.java
+++ src/main/java/com/example/demo/entity/File.java
@@ -1,10 +1,12 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+
 
 @Data
 @Entity
